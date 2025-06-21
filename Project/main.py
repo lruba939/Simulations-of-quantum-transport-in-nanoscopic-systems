4654f926db47afc8994c4eebe2131016229d7890
0ruba <lukaszruba939@gmail.com>
2025-06-21 18:13:45 +0200

    final tests for Z and P

--- Project/main.py
+++ Project/main.py
@@ -25,37 +25,60 @@ def main():
     # plt.plot(x, delta)
     # plt.show()
     
-    energies = np.linspace(0, 0.5e-3, 100)
+    # energies = np.linspace(0, 0.5e-3, 100)
 
-    R_ee = []
-    R_he = []
-    T = []
+    # R_ee = []
+    # R_he = []
+    # T = []
 
-    for E in energies:
-        Ree, Rhe = reflection_coeffs(E)
-        T_val = transmission(E) / 2
-        R_ee.append(Ree)
-        R_he.append(Rhe)
-        T.append(T_val)
+    # for E in energies:
+    #     Ree, Rhe = reflection_coeffs(E)
+    #     T_val = transmission(E) / 2
+    #     R_ee.append(Ree)
+    #     R_he.append(Rhe)
+    #     T.append(T_val)
 
-    plt.plot(energies*1e3, R_ee, label="$R_{ee}$")
-    plt.plot(energies*1e3, R_he, label="$R_{he}$")
-    plt.plot(energies*1e3, T, label="$T$")
-    plt.xlabel("E [meV]")
-    plt.ylabel("Value")
-    plt.legend(loc='best')
-    plt.show()
+    # plt.plot(energies*1e3, R_ee, label="$R_{ee}$")
+    # plt.plot(energies*1e3, R_he, label="$R_{he}$")
+    # plt.plot(energies*1e3, T, label="$T$")
+    # plt.xlabel("E [meV]")
+    # plt.ylabel("Value")
+    # plt.legend(loc='best')
+    # plt.show()
 
-    G = []
-    for E in energies:
-        Ree, Rhe = reflection_coeffs(E)
-        G_val = 1 - Ree + Rhe
-        G.append(G_val)
+    ## Final test
+    
+    # for Z in [0.0, 0.5, 1.0, 1.5]:
+    #     p.Z = Z
+    #     G = []
+    #     for E in energies:
+    #         Ree, Rhe = reflection_coeffs(E)
+    #         G_val = 1 - Ree + Rhe
+    #         G.append(G_val)
+    #     plt.plot(energies.T*1e3, G, label=f"Z = {Z}")
 
-    plt.plot(energies*1e3, G)
-    plt.xlabel("E [meV]")
-    plt.ylabel("Conductance [2e²/h]")
-    plt.show()
+    # plt.xlabel("E [meV]")
+    # plt.ylabel("Conductance [2e²/h]")
+    # plt.xlim([0.0,0.5])
+    # plt.legend(loc="best")
+    # plt.show()
+    
+    # p.reset_to_defaults()
+    
+    # for P in [0.0, 0.5, 0.8, 0.99]:
+    #     p.P = P
+    #     G = []
+    #     for E in energies:
+    #         Ree, Rhe = reflection_coeffs(E)
+    #         G_val = 1 - Ree + Rhe
+    #         G.append(G_val)
+    #     plt.plot(energies.T*1e3, G, label=f"P = {P}")
+
+    # plt.xlabel("E [meV]")
+    # plt.ylabel("Conductance [2e²/h]")
+    # plt.xlim([0.1,0.5])
+    # plt.legend(loc="best")
+    # plt.show()
     
         
 if __name__ == "__main__":
